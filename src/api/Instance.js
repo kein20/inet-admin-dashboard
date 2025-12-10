b6688b369590a336b2385c4f84f2c45d0ee2b5cb
kein20 <nurlintang972@gmail.com>
2025-12-10 13:33:07 +0700

    fix error

--- src/api/Instance.js
+++ src/api/Instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'inet-admin-dashboard-production.up.railway.app',
+    baseURL: 'https://inet-admin-dashboard-production.up.railway.app',
 });
 
 export default api;
